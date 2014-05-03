7fb4a88041675c3b4684a484735393d886266b1a
Francisco Marques <kiko96kiko@gmail.com>
2014-05-03 15:33:17 +0100

    Auto select added and copy me cleared by X button

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 //by Tofran
 function edit() { 
-	var entry= document.getElementById("textbox1").value;
+	var entry = document.getElementById("textbox1").value;
 	//Remove spaces
 	var edit1 = entry.replace(" ", "");
 	//Remove  http://
@@ -13,18 +13,24 @@ function edit() {
 	var textbox2 = document.getElementById('textbox2');
 	textbox2.value = answer;
 
+	//select text
+	document.getElementById('textbox2').select();
+	//Display copy me thingy
 	document.getElementById('copyMe').style.display = "block";
 	document.getElementById('arrow').style.display = "block";
 } 
 
 function cleanTextBox(){
+	//clean textboxes
 	document.getElementById("textbox1").value = '';
 	document.getElementById("textbox2").value = '';
+	//hide "Copy me!"
+	document.getElementById('copyMe').style.display = "none";
+	document.getElementById('arrow').style.display = "none";
 }
 
 function openlink()
 {
-
 var link= document.getElementById("textbox2").value;
 var linkToOpen= "http://" + link;
 window.open(linkToOpen);
