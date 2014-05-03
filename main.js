a9af879a028cc01197525599461b44c6faf95ec0
Francisco Marques <kiko96kiko@gmail.com>
2014-05-03 14:26:32 +0100

    Improved code

---
+++ main.js
@@ -0,0 +1,28 @@
+//by Tofran
+function edit() { 
+	var entry= document.getElementById("textbox1").value;
+	//Remove spaces
+	var edit1 = entry.replace(" ", "");
+	//Remove  http://
+	var edit1 = entry.replace("http://", "");
+	//Remove "torrent/"
+	var edit2 = edit1.replace("torrent/", "");
+	//Add "torrents." and ".torrent"
+	var answer = "torrents." + edit2 + ".torrent"
+
+	var textbox2 = document.getElementById('textbox2');
+	textbox2.value = answer;
+} 
+
+function cleanTextBox(){
+	document.getElementById("textbox1").value = '';
+	document.getElementById("textbox2").value = '';
+}
+
+function openlink()
+{
+
+var link= document.getElementById("textbox2").value;
+var linkToOpen= "http://" + link;
+window.open(linkToOpen);
+}
